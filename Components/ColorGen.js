26d738c1df0fb26e988baa520c9beb46a60cdd12
Deep Patel <deeppatel3543@gmail.com>
2022-01-06 11:31:35 +1000

    Created React App for Color Image generator

---
+++ Components/ColorGen.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ColorGen = () => {
+    return(
+        // Image - Container
+        <div className="img-container">
+            {/* Title */}
+            <h2 className="header">Discrete Colors Image [32,768 colors]</h2>
+            {/* Add Color Generator Code */}
+
+            {/* Add functionality to shuffle colors in the image */}
+        </div>
+    )
+}
